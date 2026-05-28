046870deab95a7a349958157a97ffadb31156209
kaen25 <kaen25@gmail.com>
2026-05-28 15:55:50 +0200

    chore: package scaffold

---
+++ vitest.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from "vitest/config";
+
+export default defineConfig({
+	test: {
+		coverage: {
+			provider: "v8",
+			include: ["src/**"],
+			exclude: ["src/**/*.d.ts"],
+			reporter: ["text-summary", "json-summary"],
+			thresholds: {
+				lines: 54,
+				statements: 53,
+				branches: 41,
+				functions: 56,
+			},
+		},
+	},
+});
